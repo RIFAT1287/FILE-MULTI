3e26e88ce88f5b8121a1bfdd668ed697a212d37b
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2023-11-18 22:17:45 +0600

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://biamwnuw:pMMHUaRvC5dYHu2Or4WjX2nMaz2wlAZU@bubble.db.elephantsql.com/biamwnuw")
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001980741828"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001582904067"))
