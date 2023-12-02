7fdb6d13f1a859ee0f74a79050529a52870a9b49
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2023-12-02 12:47:23 +0600

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://biamwnuw:pMMHUaRvC5dYHu2Or4WjX2nMaz2wlAZU@bubble.db.elephantsql.com/biamwnuw")
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001980741828"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001866668533"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001942340123"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
